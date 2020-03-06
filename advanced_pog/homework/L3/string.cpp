0fc87b9bf2ba4f0395ff9d354ed34a8cbb9dea73
Matrix <2500223255@qq.com>
2020-03-06 16:11:44 +0900

    finish my string.cpp

--- advanced_pog/homework/L3/string.cpp
+++ advanced_pog/homework/L3/string.cpp
@@ -24,15 +24,47 @@ public:
             length = 0;
         }
     }
-    int length(){
+    int get_length() const{
         return length;
     }
-    int stradd(const mystring &a, const mystring &b, mystring &d){};
+    friend int stradd(const mystring &a, const mystring &b, mystring &d);
+    int print(){
+        for(int i = 0; s[i]; i++)
+            cout << s[i];
+        return 0;
+    }
 };
 
-int mystring:: stradd(const mystring &a, const mystring &b, mystring &d){
+int stradd(const mystring &a, const mystring &b, mystring &d){
     if(d.s != NULL){
         d.~mystring();
     }
-    
+    int newlength = a.get_length() + b.get_length();
+    d = new char[newlength+1];
+    int count = 0;
+    for(int i = 0; a.s[i]; i++){
+        d.s[count++] = a.s[i];
+    }
+    for(int i = 0; b.s[i]; i++){
+        d.s[count++] = b.s[i];
+    }
+    d.s[newlength] = '\0';
+    d.length = newlength;
+    return newlength;
+}
+
+int main(){
+    char a[] = "Hello World\n";
+    char b[] = "Hello C++\n";
+    mystring s1(a);
+    mystring s2(b);
+    s1.print();
+    cout << "The length of the s1 is " << s1.get_length() << endl;
+    s2.print();
+    cout << "The length of the s2 is " << s2.get_length() << endl;
+    mystring s;
+    stradd(s1, s2, s);
+    s.print();
+    cout << "The length of the s is " << s.get_length() << endl;
+    return 0;
 }
