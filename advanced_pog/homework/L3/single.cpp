68496ad040db288f0286a484377c328c4778cb1e
Matrix <2500223255@qq.com>
2020-03-06 22:25:41 +0800

    finish my single.cpp

--- advanced_pog/homework/L3/single.cpp
+++ advanced_pog/homework/L3/single.cpp
@@ -3,14 +3,28 @@ using namespace std;
 
 class single{
     static int count;
-    static single *root;
 public:
     single(){
-        
-
+        count++;
     }
+    ~single(){
+        count--;
+    }
+    friend single* creat();
 };
 
+single *root = NULL;
+
+single* creat(){
+    if(single:: count == 0){
+        single *p = new single;
+        root = p;
+        return p;
+    }
+    else{
+        return root;
+    }
+}
 
 /*
 合理运用C++提供的各种机制，定义一个类Single，使得程序中只能创建一个该类的对象。
