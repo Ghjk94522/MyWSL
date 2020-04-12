2a77245b5fe41c03f2042093745de51194663666
Matrix <2500223255@qq.com>
2020-04-13 00:00:23 +0800

    automaticlaly commit

--- algorithm/L3_2.cpp
+++ algorithm/L3_2.cpp
@@ -37,6 +37,36 @@ void visit(node *root, int &max)
     visit(root->right, max);
 }
 
+int head[N] = {0};
+int ver[N] = {0};
+int Next[N] = {0};
+int edge[N] = {0};
+bool v[N];
+int tot;
+
+// the dp to calculate the d of the tree
+void add(int x, int y, int z){
+    ver[++tot] = y;
+    edge[tot] = z;
+    Next[tot] = head[x];
+    head[x] = tot;
+}
+
+int ans;
+int d[N] = {0};
+
+void dp(int x){
+    v[x] = true;
+    for(int i = head[x]; i; i = Next[i]){
+        int y = ver[i];
+        if(v[y]) continue;
+        dp(y);
+        if(d[x] + d[y] + edge[i] > ans) ans = d[x] + d[y] + edge[i];
+        if(d[y] + edge[i] > d[x]) d[x] = d[y] + edge[i];
+    }
+}
+
+
 int main()
 {
     int n;
@@ -58,6 +88,8 @@ int main()
     int max = -1;
     visit(root, max);
     int d = -1;
+    tot = 0; ans = 0;
+    for(int i = 0; i <= n; i++) v[i] = false;
     cout << max << endl
          << d << endl;
     return 0;
