01d676cd06da2951a172387269d830741d4069f3
Matrix <2500223255@qq.com>
2020-07-24 12:49:45 +0900

    finish the 1255 luogu

--- Oj_luogu/done/P1255.cpp
+++ Oj_luogu/done/P1255.cpp
@@ -33,3 +33,28 @@ int main(){
 */
 
 /// according to the answers in luogu.com
+int n, len = 1, f[5005][5005];
+
+void hp(int k){
+    int i;
+    for(i = 1; i < len; i++)
+        f[k][i] = f[k-1][i] + f[k-2][i];
+    for(i = 1; i < len; i++){
+        if(f[k][i] >= 10){
+            f[k][i+1] += f[k][i]/10;
+            f[k][i] %= 10;
+            if(f[k][len+1]) len++;
+        }
+    }
+}
+
+int main(){
+    cin >> n;
+    int i;
+    f[1][1] = 1; f[2][1] = 2;
+    for(i = 3; i < n; i++)
+        hp(i);
+    for(i = len; i >= 1; i--)
+        cout << f[n][i];
+    return 0;
+}
