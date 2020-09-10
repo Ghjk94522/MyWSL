d15cc053c643227b5fcf50f0af060132b7543300
Matrix <2500223255@qq.com>
2020-09-10 21:27:04 +0900

    add a 40.py in leetcode dir

---
+++ leetcode/40.py
@@ -0,0 +1,41 @@
+'''
+给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的每个数字在每个组合中只能使用一次。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/combination-sum-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+
+'''
+递归  回溯
+'''
+
+
+from typing import List
+import collections
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def dfs(pos: int, rest: int):
+            nonlocal sequence
+            if rest == 0:
+                ans.append(sequence[:])
+                return
+            if pos == len(freq) or rest < freq[pos][0]:
+                return 
+            
+            dfs(pos+1, rest)
+
+            most = min(rest//freq[pos][0], freq[pos][1])
+            for i in range(1, most+1):
+                sequence.append(freq[pos][0])
+                dfs(pos+1, rest-i*freq[pos][0])
+            sequence = sequence[:-most]
+
+        freq = sorted(collections.Counter(candidates).items())
+        ans = list()
+        sequence = list()
+        dfs(0, target)
+        return ans 
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-test = []
-test.append(1)
-test.append(2)
-print(test)
+import collections
+
+ans = [1, 2, 3, 5, 7, 8, 9, 11, 4, 6]
+res = sorted(collections.Counter(ans).items())
+print(res)
