de4dc2bb7fac5baa18a44ad728f23f23193a0c0e
Matrix <2500223255@qq.com>
2020-09-11 22:08:30 +0900

    add a readme for numQuestion in leetcode

---
+++ leetcode/numQuestion/300.py
@@ -0,0 +1,49 @@
+'''
+给定一个无序的整数数组，找到其中最长上升子序列的长度。
+
+比如：
+输入: [10,9,2,5,3,7,101,18]
+输出: 4 
+解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+
+'''
+
+''' 
+dp O(n^2)
+'''
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        dp = []
+        for i in range(len(nums)):
+            dp.append(1)
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j]+1)
+        return max(dp)
+
+
+'''
+greedy O(nlogn)
+'''
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        d = []
+        for n in nums:
+            if not d or n > d[-1]:
+                d.append(n)
+            else:
+                l, r = 0, len(d) - 1
+                loc = r
+                while l <= r:
+                    mid = (l + r) // 2
+                    if d[mid] >= n:
+                        loc = mid 
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                d[loc] = n 
+        return len(d)
