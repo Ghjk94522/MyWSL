c8207d689e6eb5fcff6e9cb94dd91909765c6840
Matrix <2500223255@qq.com>
2020-09-19 01:13:51 +0900

    add a new folder to record my lecture on github about web

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
+'''
+Author: Coconut
+Date: 2020-09-10 21:26:21
+LastEditTime: 2020-09-18 20:19:12
+LastEditors: Coconut
+Description: 
+FilePath: /myFile/myWSL/test.py
+可以输入预定的版权声明、个性签名、空行等
+'''
 import collections
 
 ans = [1, 2, 3, 5, 7, 8, 9, 11, 4, 6]
