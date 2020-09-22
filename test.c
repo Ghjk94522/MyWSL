d399e8028195ac6d3199f048a8bd651add103725
Matrix <2500223255@qq.com>
2020-09-22 18:06:54 +0900

    add a new test for pthread

--- test.c
+++ test.c
@@ -1,13 +1,28 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<pthread.h>
 
-__attribute__((constructor)) void hello() {
-	printf("Hello, World!\n");
-}
+int g = 0; // a global variable can be changed in threads
 
-// See also : atexit(3)
-__attribute__((destructor)) void goodbye() {
-	printf("Goodbye, Cruel OS World!\n");
+void *myThreadFun(void *vargp){
+	// Store the value argument passed to this thread
+	int *myid = (int *)vargp;
+	// Let us create a static variable to observe its changes
+	static int s = 0;
+	// Change the static and global variables
+	++s; ++g;
+	// Print the arguments, static and global variables
+	printf("Thread ID: %d, Static: %d, Global: %d\n", *myid, ++s, ++g);
 }
 
 int main(){
+	int i;
+	pthread_t tid;
+	// Let us creat three threads
+	for( i = 0; i < 3; i++ )
+		pthread_create(&tid, NULL, myThreadFun, (void *)&tid);
+
+	pthread_exit(NULL);
+	return 0;
 }
