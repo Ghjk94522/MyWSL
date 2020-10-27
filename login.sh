b5e48819c4d68d79e674fff84ca9ef79c439195a
Matrix <2500223255@qq.com>
2020-10-27 09:39:15 +0800

    final push from ubuntu

---
+++ login.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -d "username=181860126&password=yuio1235789" \
+  http://p.nju.edu.cn/portal_io/login 
---
+++ transfer/test.py
@@ -0,0 +1,3 @@
+import akshare as ak
+op_df = ak.option_finance_board(symbol="华夏上证50ETF期权", end_month="2019")
+print(op_df)
