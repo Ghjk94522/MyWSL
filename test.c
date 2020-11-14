40bb7a99c0480ca9b7c2716b92f2b9cb03baa2c2
root <root@DESKTOP-O7A6QNL.localdomain>
2020-11-14 20:14:26 +0800

    regularly commit

--- test.c
+++ test.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+#include<stdint.h>
+
+float Q_rsqrt( float number ) {
+  union { float f; uint32_t i; } conv;
+  float x2 = number * 0.5F;
+  conv.f = number;
+  conv.i = 0x5f3759df - ( conv.i >> 1 ); // ???
+  conv.f = conv.f * ( 1.5F - ( x2 * conv.f * conv.f ) );
+  return conv.f;
+}
 
 int main(){
-	float a = 0302 / 020;	
-	printf("res is %o\n", a);
+	float res = Q_rsqrt(4);
+	printf("res is %f\n", res);
 	return 0;
 }
